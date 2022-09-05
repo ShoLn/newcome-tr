b56bce17b3afddead10a164a6ea89be4ae434aff
Shawn Lin <T22035@Oring.com.tw>
2022-09-05 14:33:35 +0800

    formik practice

--- src/pages/react-formik/index.js
+++ src/pages/react-formik/index.js
@@ -7,20 +7,55 @@ import Checkbox from "@mui/material/Checkbox";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 import { useFormik } from "formik";
+
+// 表單初始值
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+// 提交表單
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+// 驗證涵式
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.email) {
+    errors.email = "Name is required";
+  } else if (
+    !/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/.test(
+      values.email
+    )
+  ) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length <= 6) {
+    errors.password = "Password length must greater or equal to 6";
+  }
+
+  return errors;
+};
 
 function ReactFormikPage() {
   const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   return (
-    <Box
+    <Paper
       sx={{
         marginTop: 25,
+        padding: 5,
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
@@ -33,10 +68,16 @@ function ReactFormikPage() {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" noValidate sx={{ mt: 1 }}>
+      <Box
+        component="form"
+        noValidate
+        sx={{ mt: 1 }}
+        onSubmit={formik.handleSubmit}
+      >
         <TextField
-          onChange={formik.handleChange}
-          value={formik.values.email}
+          {...formik.getFieldProps("email")}
+          error={Boolean(formik.touched.email && formik.errors.email)}
+          helperText={formik.touched.email ? formik.errors.email : ""}
           margin="normal"
           required
           fullWidth
@@ -47,8 +88,9 @@ function ReactFormikPage() {
           autoFocus
         />
         <TextField
-          onChange={formik.handleChange}
-          value={formik.values.password}
+          {...formik.getFieldProps("password")}
+          error={Boolean(formik.touched.password && formik.errors.password)}
+          helperText={formik.touched.password ? formik.errors.password : ""}
           margin="normal"
           required
           fullWidth
@@ -71,7 +113,7 @@ function ReactFormikPage() {
           Sign In
         </Button>
       </Box>
-    </Box>
+    </Paper>
   );
 }
 
